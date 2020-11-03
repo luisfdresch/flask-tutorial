7d732f445374384c0f6ede3f8237fb1b18108cf6
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-03 20:59:04 -0300

    Add comment

--- flaskr/db.py
+++ flaskr/db.py
@@ -7,6 +7,7 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
+        # The connection is created only when necessary. Otherwise it is just reused
         g.db = sqlite3.connect(
                 current_app.config['DATABASE'],
                 detect_types = sqlite3.PARSE_DECLTYPES
