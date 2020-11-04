3ba554b23dfca2729f489f1b2a0de1b632a17c4a
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-04 15:53:00 -0300

    Add /auth/register view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,31 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request,form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+                ).fetchone() in not None:
+            error = 'User {} is already registered'.format(username)
+
+        if error is None:
+            db.execute(
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password))
+                    )
+            db.commmit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
