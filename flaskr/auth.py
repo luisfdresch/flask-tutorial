2e7ef55a02d77011c5a583af9d06455113488461
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-04 16:16:34 -0300

    Add login_required decorator

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -79,3 +79,13 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login')
+
+        return view(**kwargs)
+
+    return wrapped_view
+
