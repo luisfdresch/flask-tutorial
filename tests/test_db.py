fa345bf388851e95640324870a02921e2b2c7372
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-06 15:00:35 -0300

    Fix typo

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,7 +4,7 @@ import pytest
 from flaskr.db import get_db
 
 def test_get_close_db(app):
-    with app.app_sontext():
+    with app.app_context():
         db = get_db()
         assert db is get_db()
          
@@ -23,5 +23,5 @@ def test_init_db_command(runner, monkeypatch):
     monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
     result = runner.invoke(args=['init-db'])
     assert 'Initialized' in result.output
-    assert Record.called
+    assert Recorder.called
 
