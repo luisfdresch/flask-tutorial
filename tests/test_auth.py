66e05c7d1ea0e63e1fda4560ec55e734dc0f5082
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-06 14:47:02 -0300

    Fix typo

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -19,7 +19,7 @@ def test_register(client, app):
 @pytest.mark.parametrize(('username', 'password', 'message'), (
     ('', '', b'Username is required'),
     ('a', '', b'Password is required'),
-    ('test', 'test', b'already reistered'),
+    ('test', 'test', b'already registered'),
     ))
 def test_register_validate_input(client, username, password, message):
     response = client.post(
