77144ea256596747d53577397b3c88df55989c1f
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-06 14:57:57 -0300

    Fix typo

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,7 +3,7 @@ from flaskr import create_app
 
 def test_config():
     assert not create_app().testing
-    assert create-app({'TESTING': True}).testing
+    assert create_app({'TESTING': True}).testing
 
 
 def test_hello(client):
