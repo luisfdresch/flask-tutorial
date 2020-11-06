66f7b2ffc615aecff655927d615cbf23ca088294
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-06 12:17:33 -0300

    Fix typo

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
 def app():
     db_fd, db_path = tempfile.mkstemp()
 
-    app = crate_app({
+    app = create_app({
         'TESTING': True,
         'DATABASE': db_path,
         })
