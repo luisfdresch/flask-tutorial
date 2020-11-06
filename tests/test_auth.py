5a56bb3a2d8ba8fac7f2529a48a64f9da796fd81
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-06 15:47:35 -0300

    Add login tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -16,6 +16,26 @@ def test_register(client, app):
                 ).fetchone() is not None
 
 
+def test_login(client, auth):
+    assert client.get('/auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers['Location'] == 'http://localhost/'
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == test
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username'),
+    ('test', 'a', b'Incorrect password'),
+    ))
+def test_login_validate_input(auth, username, password, message):
+    response auth.login(username, password)
+    assert message in response.data
+
+
 @pytest.mark.parametrize(('username', 'password', 'message'), (
     ('', '', b'Username is required'),
     ('a', '', b'Password is required'),
