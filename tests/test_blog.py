6ef250fc3fa4ed7ddd779f8ec0e2949ece8a1f2c
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-11-06 14:48:42 -0300

    Fix typo

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -10,7 +10,7 @@ def test_index(client, auth):
     response = client.get('/')
     assert b'Log Out' in response.data
     assert b'test title' in response.data
-    assert b'by test ono 2018-01-01' in response.data
+    assert b'by test on 2018-01-01' in response.data
     assert b'test\nbody' in response.data
     assert b'href="/1/update"' in response.data
 
